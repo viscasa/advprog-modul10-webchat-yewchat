d6ba3f759da4ea4b331b5af737a07b4c31b728bb
Attar <attaralwie@gmail.com>
2025-05-22 13:08:26 +0700

    feat: init commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,17 @@
 #![recursion_limit = "512"]
+use std::cell::RefCell;
+use std::rc::Rc;
+pub mod services;
+pub type User = Rc<UserInner>;
+
+#[derive(Debug, PartialEq)]
+pub struct UserInner {
+    pub username: RefCell<String>,
+}
+
+mod components;
+use components::login::Login;
+use components::chat::Chat;
 
 use wasm_bindgen::prelude::*;
 use yew::functional::*;
@@ -13,3 +26,48 @@ use yew_router::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[derive(Debug, Clone, Copy, PartialEq, Routable)]
+pub enum Route {
+    #[at("/")]
+    Login,
+    #[at("/chat")]
+    Chat,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(selected_route: &Route) -> Html {
+    match selected_route {
+        Route::Login => html! {<Login />},
+        Route::Chat => html! {<Chat/>},
+        Route::NotFound => html! {<h1>{"404 baby"}</h1>},
+    }
+}
+
+#[function_component(Main)]
+fn main() -> Html {
+
+    let ctx = use_state(|| {
+        Rc::new(UserInner {
+            username: RefCell::new("initial".into()),
+        })
+    });
+
+    html! {
+        <ContextProvider<User> context={(*ctx).clone()}>
+        <BrowserRouter>
+            <div class="flex w-screen h-screen">
+                <Switch<Route> render={Switch::render(switch)}/>
+            </div>
+        </BrowserRouter>
+        </ContextProvider<User>>
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn run_app() -> Result<(), JsValue> {
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<Main>();
+    Ok(())
+}
